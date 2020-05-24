fa657621382b540fa2ad8805946de8c1655bdc5e
Shubham Badgujar <35478603+shubham8550@users.noreply.github.com>
2020-05-24 20:31:21 +0530

    desable batt optmization

--- app/src/main/java/com/basementgeniusstudios/botmaster/ui/HomeActivity.java
+++ app/src/main/java/com/basementgeniusstudios/botmaster/ui/HomeActivity.java
@@ -5,7 +5,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.view.View;
 
 import com.basementgeniusstudios.botmaster.R;
@@ -23,5 +27,17 @@ public class HomeActivity extends AppCompatActivity {
     }
     public void openNotificationSettings(View view) {
         startActivity(new Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS));
+    }
+    public void openBatterOpt(View view) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent();
+            String packageName = getPackageName();
+            PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+                intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                intent.setData(Uri.parse("package:" + packageName));
+                startActivity(intent);
+            }
+        }
     }
 }
